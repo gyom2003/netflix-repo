b919c931d75cfb8ff09a48c1f9455bef79a91d11
gyom2003 <guillaume1.leformal@gmail.com>
2022-03-19 20:43:22 +0100

    partie 4 du projet premier push

--- js/main.js
+++ js/main.js
@@ -18,23 +18,47 @@ function general() {
 }
 
 
-//
+//https://github.com/Toumi6796/netflix-clone-tailwind/blob/main/index.html
 function rightScroll(carousel, rightArrow) {
     const scroll = document.querySelector(carousel);
-
     const right = document.querySelector(rightArrow);
-    
-    right.addEventListener("click", () => {
-        scroll.scrollLeft += scroll.offsetWidth;
-    })
+    scroll.scrollLeft += scroll.offsetWidth;
+
 }
 
 function leftScroll(carousel, leftArrow) {
     const scroll = document.querySelector(carousel);
     const left = document.querySelector(leftArrow);
+    scroll.scrollLeft -= scroll.offsetWidth;
 
-    left.addEventListener("click", () => {
-        scroll.scrollLeft -= scroll.offsetWidth;
-    })
-    
 }
+
+function rightScroll2(carousel2, rightArrow2) {
+    const scroll = document.querySelector(carousel2);
+    const right = document.querySelector(rightArrow2);
+    scroll.scrollLeft += scroll.offsetWidth;
+
+}
+
+function leftScroll2(carousel2, leftArrow2) {
+    const scroll = document.querySelector(carousel2);
+    const left = document.querySelector(leftArrow2);
+    scroll.scrollLeft -= scroll.offsetWidth;
+
+}   
+
+function rightScroll3(carousel3, rightArrow3) {
+    const scroll = document.querySelector(carousel3);
+    const right = document.querySelector(rightArrow3);
+    scroll.scrollLeft += scroll.offsetWidth;
+
+}
+
+function leftScroll3(carousel3, leftArrow3) {
+    const scroll = document.querySelector(carousel3);
+    const left = document.querySelector(leftArrow3);
+    scroll.scrollLeft -= scroll.offsetWidth;
+
+}
+
+
